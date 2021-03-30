382eea1745ea6985afa6c8f3691d4dd80f076e5c
Mario Santucci <mario.santucci95@gmail.com>
2021-03-30 16:17:59 +0200

    aggiunta parte logica e stampa prezzo finaleburger

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
+//Il programma dovrà consentire di calcolare il prezzo del panino selezionando o deselezionando gli ingredienti proposti
+//
+//tutto il calcolo avviene all'attivazione del pulsante 'calculate'
 
+var calculateButton = document.getElementById('calculate');
 
+calculateButton.addEventListener('click', function() {
+
+    var prezzoBaseBurger = 50;
+  
+    var ingredientsCheckboxes = document.getElementsByClassName('ingredients');
+    
+    for( var i = 0; i < ingredientsCheckboxes.length; i++ ) {
+        var checkBox = ingredientsCheckboxes[i];
+        
+        if ( checkBox.checked == true ) {
+            var ingredientsPrice = parseInt(checkBox.value);
+
+            prezzoBaseBurger += ingredientsPrice;
+
+        }
+    }
+
+    console.log(prezzoBaseBurger);
+    document.getElementById('final-price').innerHTML = 'il tuo prezzo è ' + prezzoBaseBurger.toFixed(2) + '$';
+
+});
 
 
 
